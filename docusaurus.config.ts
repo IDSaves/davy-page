c20536651f216a265bfbf74930acefc8a33b5daa
Ivan Davydov <davydoff33@yandex.ru>
2023-12-26 21:08:08 +0300

    git repo links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,10 +27,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/IDSaves/davy-page/',
         },
 
         theme: {
@@ -57,7 +55,7 @@ const config: Config = {
           label: 'Записи',
         },
         {
-          href: 'https://github.com/IDSaves',
+          href: 'https://github.com/IDSaves/davy-page',
           label: 'GitHub',
           position: 'right',
         },
